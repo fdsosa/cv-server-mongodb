4758692c7d16428c36e3301bb6e319d355e94af4
Federico <fdsosa.35@gmail.com>
2019-10-08 20:26:47 -0300

    Deleted cors origin

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.set('port', process.env.PORT || 3200);
 
 // Middlewares    FIX!!
 //app.use(cors({origin: 'http://localhost:4200'}));
-app.use(cors({origin: 'https://proyect-cv-angular.herokuapp.com/'}));
+app.use(cors());
 
 app.use(express.json());
 
